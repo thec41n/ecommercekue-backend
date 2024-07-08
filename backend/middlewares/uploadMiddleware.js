2abf024db4bba17d6c0f82d533f8b213044aa476
MikhaelEmpi <d.mikhaelempi@gmail.com>
2024-07-08 21:20:42 +0700

    gcp file storage upload

---
+++ backend/middlewares/uploadMiddleware.js
@@ -0,0 +1,85 @@
+import { Storage } from "@google-cloud/storage";
+import multer from "multer";
+import path from "path";
+import sharp from "sharp";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const keyFilename =
+  process.env.GCP_KEYFILE_PATH ||
+  path.join(__dirname, "../config/kueyanti-project-d52f6867be18.json");
+
+  const storage = new Storage({ keyFilename });
+  const bucketName = 'uploads-kueyanti';
+  
+  const bucket = storage.bucket(bucketName);
+  
+  const multerStorage = multer.memoryStorage();
+  
+  const upload = multer({
+    storage: multerStorage,
+    fileFilter: (req, file, cb) => {
+      if (file.mimetype.startsWith('image')) {
+        cb(null, true);
+      } else {
+        cb(new Error('Not an image! Please upload an image.', 400), false);
+      }
+    }
+  });
+  
+  const getFormattedDate = () => {
+    const date = new Date();
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+  
+    return `${day}${month}${year}-${hours}${minutes}${seconds}`;
+  };
+  
+  const uploadToGCS = async (req, res, next) => {
+    if (!req.file) {
+      console.log('No file received');
+      return res.status(400).json({ message: 'No file uploaded' });
+    }
+  
+    const formattedDate = getFormattedDate();
+    const extension = path.extname(req.file.originalname);
+    const filename = `${formattedDate}${extension}`;
+    const blob = bucket.file(filename);
+    const blobStream = blob.createWriteStream();
+  
+    try {
+      // Use sharp to resize the image
+      const resizedImageBuffer = await sharp(req.file.buffer)
+        .resize(1920, 1080)
+        .toBuffer();
+  
+      blobStream.on('error', err => {
+        console.error('Error during upload:', err);
+        req.file.cloudStorageError = err;
+        next(err);
+      });
+  
+      blobStream.on('finish', () => {
+        req.file.cloudStorageObject = blob.name;
+        req.file.cloudStoragePublicUrl = getPublicUrl(blob.name);
+        console.log('File is accessible at:', req.file.cloudStoragePublicUrl);
+        next();
+      });
+  
+      blobStream.end(resizedImageBuffer);
+    } catch (err) {
+      console.error('Error during image processing or upload:', err);
+      next(err);
+    }
+  };
+  
+  const getPublicUrl = (filename) => {
+    return `https://storage.googleapis.com/${bucketName}/${filename}`;
+  };
+  
+  export { upload, uploadToGCS };
--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -2,62 +2,79 @@ import path from "path";
 import express from "express";
 import multer from "multer";
 import sharp from "sharp";
+import { upload, uploadToGCS } from '../middlewares/uploadMiddleware.js';
+import { authenticate, authorizeAdmin } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
-  filename: (req, file, cb) => {
-    const extname = path.extname(file.originalname);
-    cb(null, `${file.fieldname}-${Date.now()}${extname}`);
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "uploads/");
+//   },
+//   filename: (req, file, cb) => {
+//     const extname = path.extname(file.originalname);
+//     cb(null, `${file.fieldname}-${Date.now()}${extname}`);
+//   },
+// });
 
-const fileFilter = (req, file, cb) => {
-  const filetypes = /jpe?g|png|webp/;
-  const mimetypes = /image\/jpe?g|image\/png|image\/webp/;
+// const fileFilter = (req, file, cb) => {
+//   const filetypes = /jpe?g|png|webp/;
+//   const mimetypes = /image\/jpe?g|image\/png|image\/webp/;
 
-  const extname = path.extname(file.originalname).toLowerCase();
-  const mimetype = file.mimetype;
+//   const extname = path.extname(file.originalname).toLowerCase();
+//   const mimetype = file.mimetype;
 
-  if (filetypes.test(extname) && mimetypes.test(mimetype)) {
-    cb(null, true);
+//   if (filetypes.test(extname) && mimetypes.test(mimetype)) {
+//     cb(null, true);
+//   } else {
+//     cb(new Error("Mohon upload file gambar!"), false);
+//   }
+// };
+
+// const upload = multer({ storage, fileFilter });
+// const uploadSingleImage = upload.single("image");
+
+// router.post("/", (req, res) => {
+//   uploadSingleImage(req, res, async (err) => {
+//     if (err) {
+//       res.status(400).send({ message: err.message });
+//     } else if (req.file) {
+//       try {
+//         const outputPath = `uploads/res-${req.file.filename}`;
+//         await sharp(req.file.path)
+//           .resize(1920, 1080)
+//           .toFile(outputPath);
+
+//         res.status(200).send({
+//           message: "Gambar berhasil diupload!",
+//           image: `/${outputPath}`,
+//         });
+//       } catch (error) {
+//         console.error("Error resizing image");
+//         res.status(500).send({
+//           message: "Gagal mengubah ukuran gambar",
+//           error: error.message,
+//         });
+//       }
+//     } else {
+//       res.status(400).send({ message: "Tidak ada file gambar terlampir" });
+//     }
+//   });
+// });
+
+router.post('/', authenticate, upload.single('image'), uploadToGCS, (req, res) => {
+  console.log('Route handler called');
+  if (req.file && req.file.cloudStoragePublicUrl) {
+    res.status(200).json({
+      message: 'Upload was successful',
+      url: req.file.cloudStoragePublicUrl
+    });
   } else {
-    cb(new Error("Mohon upload file gambar!"), false);
+    res.status(500).json({
+      message: 'Something went wrong'
+    });
   }
-};
-
-const upload = multer({ storage, fileFilter });
-const uploadSingleImage = upload.single("image");
-
-router.post("/", (req, res) => {
-  uploadSingleImage(req, res, async (err) => {
-    if (err) {
-      res.status(400).send({ message: err.message });
-    } else if (req.file) {
-      try {
-        const outputPath = `uploads/res-${req.file.filename}`;
-        await sharp(req.file.path)
-          .resize(1920, 1080)
-          .toFile(outputPath);
-
-        res.status(200).send({
-          message: "Gambar berhasil diupload!",
-          image: `/${outputPath}`,
-        });
-      } catch (error) {
-        console.error("Error resizing image");
-        res.status(500).send({
-          message: "Gagal mengubah ukuran gambar",
-          error: error.message,
-        });
-      }
-    } else {
-      res.status(400).send({ message: "Tidak ada file gambar terlampir" });
-    }
-  });
 });
+
 
 export default router;
