83bf31ff61d25e079bb49fb0721d19498bdb6ecb
MikhaelEmpi <d.mikhaelempi@gmail.com>
2024-07-08 14:53:54 +0700

    fix

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -1,19 +1,20 @@
 import jwt from "jsonwebtoken";
 
 const generateToken = (res, userId) => {
-    const token = jwt.sign({userId}, process.env.JWT_SECRET,
-        {expiresIn: "30d",
-    });
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
 
-    // Set JWT as HTTP-Only Cookie
-    res.cookie('jwt', token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV !== 'development',
-        sameSite: 'None',
-        maxAge: 30 * 24 * 60 * 60 * 1000
-    })
+  console.log("Generated Token:", token); // Debug log
 
-    return token
+  res.cookie('jwt', token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== 'development',
+    sameSite: 'strict',
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+  });
+
+  return token;
 };
 
 export default generateToken;
