b39a545c4e672268b804424c81bb639b16ed0ae6
MikhaelEmpi <d.mikhaelempi@gmail.com>
2024-07-08 13:45:36 +0700

    fix

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from 'cors';
+import path from "path";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
