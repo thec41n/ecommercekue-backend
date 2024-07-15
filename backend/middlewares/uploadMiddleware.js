e4e0269913f82c955fc9dd456133f2a2387985e4
MikhaelEmpi <d.mikhaelempi@gmail.com>
2024-07-15 14:37:39 +0700

    fs import

--- backend/middlewares/uploadMiddleware.js
+++ backend/middlewares/uploadMiddleware.js
@@ -3,6 +3,7 @@ import multer from "multer";
 import path from "path";
 import { fileURLToPath } from "url";
 import sharp from "sharp";
+import fs from "fs";
 import dotenv from "dotenv";
 
 dotenv.config();
