c6efa1353b91622065d4957400de3ca178da7705
MikhaelEmpi <d.mikhaelempi@gmail.com>
2024-08-03 11:40:52 +0700

    fix secure

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -7,8 +7,7 @@ const generateToken = (res, userId) => {
 
   res.cookie('jwt', token, {
     httpOnly: true,
-    // secure: process.env.NODE_ENV === 'production',
-    secure: true,
+    secure: process.env.NODE_ENV === 'production',
     sameSite: 'None',
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
